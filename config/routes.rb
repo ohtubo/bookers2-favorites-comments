f31ddaa3723a564a859e42e9c2f2f37649785076
ohtubo <brte14020@gmail.com>
2021-07-02 05:14:56 +0000

    [Add]_comment_bookers2-favorites-comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resources :books, only: [:new, :create, :index, :show, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
